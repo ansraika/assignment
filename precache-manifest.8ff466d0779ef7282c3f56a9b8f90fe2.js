480b0437b41a46ae038dfdc4928d3fbd1de9b6ba
Anshit Raika <anshitraika@Anshits-MacBook-Air.local>
2024-01-11 23:05:54 +0530

    Updates

---
+++ precache-manifest.8ff466d0779ef7282c3f56a9b8f90fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "973f3a25dbce7637a436f40b620dd549",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "11a3ab911a7517587aa7",
+    "url": "/assignment/static/css/main.8caf3b31.chunk.css"
+  },
+  {
+    "revision": "8bca827d70f3d8b1f1bd",
+    "url": "/assignment/static/js/2.2edda431.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/assignment/static/js/2.2edda431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/assignment/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "11a3ab911a7517587aa7",
+    "url": "/assignment/static/js/main.91009bf2.chunk.js"
+  },
+  {
+    "revision": "eb8fe34970065b29ec05",
+    "url": "/assignment/static/js/runtime-main.6c56891c.js"
+  }
+]);
